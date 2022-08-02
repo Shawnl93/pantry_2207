e1a44247787a2040dafb58e78c62926011f27185
Shawn Lee <Shawnl93@hotmail.com>
2022-08-02 09:19:09 -0600

    test: made test for attributes and created their methods

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,10 @@
 class Ingredient
+
+  attr_reader :name, :unit, :calories
+
+  def initialize(item)
+    @name = item[:name]
+    @unit = item[:unit]
+    @calories = item[:calories]
+  end
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -8,4 +8,16 @@ describe Ingredient do
   it "exists" do
     expect(@ingredient1).to be_instance_of(Ingredient)
   end
+
+  it "has a name" do
+    expect(@ingredient1.name).to eq("Cheese")
+  end
+
+  it "has a unit" do
+    expect(@ingredient1.unit).to eq("oz")
+  end
+
+  it "calories" do
+    expect(@ingredient1.calories).to eq(50)
+  end
 end
